62a00a4170c45a7c3cc771c6edd29728a5b80093
okb <okb.for.git@gmail.com>
2019-01-30 12:08:08 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  # get 'users/index'　コントローラー作成時に自動生成されたものたち
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'users/show'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
