60188adebccd405b6294ce6c963566f060d756f9
okb <okb.for.git@gmail.com>
2019-02-04 15:21:57 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :set_user, only: [:show, :followings, :followers]
   
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
@@ -26,7 +26,21 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
   private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end 
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
